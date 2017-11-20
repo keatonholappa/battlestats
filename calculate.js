70d3d87720d6904c8f18a4f16073a0276287c51f
Keaton Holappa <keaton.holappa@gmail.com>
2017-11-19 22:06:39 -0500

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -1,1 +1,20 @@
+/* This file contains the functions required to calculate the results of the battle simulation
+*
+*/
 
+// defines a function to get the value in every input field
+function getInputFieldValues(){
+  
+}
+
+// defines a function to roll a die
+function rollDie() {
+  
+}
+
+// defines a function to calculate the results of the battle simulation
+function calculateOutput() {
+  
+  // get the value of each input field
+  getInputFieldValues();
+}

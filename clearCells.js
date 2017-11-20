10b9020382723f77a69d0af00bb51c7041beace0
Keaton Holappa <keaton.holappa@gmail.com>
2017-11-19 21:56:13 -0500

    Update clearCells.js

--- clearCells.js
+++ clearCells.js
@@ -1,9 +1,19 @@
 function clearCells() {
 
+  // clear all of the text input fields
   var inputs, index;
-
   inputs = document.getElementsByTagName('input');
   for (index = 0; index < inputs.length; ++index) {
     inputs[index].value="";
   }
+  
+  // reset the radio buttons
+  var inputs, index;
+  inputs = document.getElementsByTagName('radioButton');
+  for (index = 0; index < inputs.length; ++index) {
+    inputs[index].checked=false;
+  }
+  
+  //set dropdowns to their default values
+  document.getElementsByTagName('dropdown').val('0');
 }

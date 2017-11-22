0b8c73abba741ab9da008cd6c4973ea01d4f7477
Keaton Holappa <keaton.holappa@gmail.com>
2017-11-21 20:40:45 -0500

    created a die roll funct

--- calculate.js
+++ calculate.js
@@ -4,12 +4,47 @@
 
 // defines a function to get the value in every input field
 function getInputFieldValues(){
+  var inputs, index, allInputs;
+  inputs = document.getElementsByTagName('input');
+  for (index = 0; index < inputs.length; ++index) {
+    allInputs[index] = inputs;
+  }
+  return allInputs;
+}
+
+// defines a function to determine if the attacker is using melee or ranged attacks
+function getAttackType() {
+  var meleeAttack, rangedAttack, attackType;
+  meleeAttack = document.getElementById('meleeAttack');
+  rangedAttack = document.getElementById('rangedAttack');
   
+  // get which one is selected
+  if (meleeAttack.checked == true) {
+    attackType = "melee";
+  } else if (rangedAttack.checked == true) {
+    attackType = "ranged";
+  }
+  return attackType;
 }
 
 // defines a function to roll a die
-function rollDie() {
+function calculateDieRoll(qty, type, modifier) { 
+  // correct for the d# value
+  type = type.replace('d','');
   
+  // ensure values are numbers
+  qty = Number(qty);
+  type = Number(type);
+  modifier = Number(modifier);
+  
+  // loop for qty and roll die/dice
+  var totalRoll = 0;
+  for (var index = 0; index < qty; ++index) {
+    var roll = Math.floor(Math.random() * type) + 1 + modifier;
+    totalRoll = Number(totalRoll + roll);
+  }
+  
+  alert(totalRoll);
 }
 
 // defines a function to calculate the results of the battle simulation
@@ -17,4 +52,10 @@ function calculateOutput() {
   
   // get the value of each input field
   getInputFieldValues();
+}
+
+
+// defines a function to write the error to errorMsg
+function writeErrorMsg(error) {
+  document.getElementById("errorMsg").innerHTML = error;
 }

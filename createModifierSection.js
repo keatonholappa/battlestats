d1d97a11b235a3aa5e5e36ee7f24dd514edfdafe
Keaton Holappa <keaton.holappa@gmail.com>
2017-11-26 22:43:54 -0500

    Update createModifierSection.js

--- createModifierSection.js
+++ createModifierSection.js
@@ -49,6 +49,9 @@ function newModifiedInputSection(divIdToAppendTo,sectionName,numModifierSections
   var lastChild = divToAppendTo.childNodes.length;
   divToAppendTo.insertBefore(accordionDiv,divToAppendTo.childNodes[lastChild]);
   
+  // remake the accordion
+  $("#accordion").append(html).accordion('destroy').accordion();
+  
   // return the new number of modifier sections
   //return numMOdifierSections;
   
